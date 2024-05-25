530353f497e52894afb6201e732e930a92bb345c
Micheal Ogden <widesvs2@gmail.com>
2024-05-24 20:35:13 -0500

    clarify code with comments, refactor Dynamo class

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
+// MALRANKS - Wides🎸
+// Bring in required Dependecies
 const express = require('express')
 const path = require("path")
-
 const dotenv = require("dotenv");
+
+//Standard config for env variables
 dotenv.config();
 
+// Slug generator
 const Dynamo = require("./dynamosecurity")
 const dynamicRoute = new Dynamo()
 let slug = dynamicRoute.create(128)
 
+//Instantiate express server
 const app = express()
 
-
-
+//Required Key
 const API_KEY = process.env.API_KEY
+
 // Use required parsers
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use(express.static(path.join(__dirname, "public/")));
-app.set("views", path.join(__dirname, "public/views"));
 
+//Set path for views and set view engine
+app.set("views", path.join(__dirname, "public/views"));
 app.set("view engine", "ejs");
 
 // Routes
+
+// GET Ranking data from MAL and return top ten
 app.get(`/slug=${slug}`, async (req, res) => {
-    console.log('~Check!~')
+    
     let url = `https://api.myanimelist.net/v2/anime/ranking?ranking_type=airing&limit=10`
     let response = await fetch(url, {
         method: 'GET',
@@ -38,9 +45,10 @@ app.get(`/slug=${slug}`, async (req, res) => {
     res.json({message: 'Wow, this actually works!', data})
 })
 
-
+// Home Page Render
 app.get('/', async (req, res) => {
     res.render('index', {title: "MAL Ranks", data: slug, status: 200})
 })
 
+//Export app to bin/www
 module.exports = app;
--- dynamosecurity.js
+++ dynamosecurity.js
@@ -1,12 +1,31 @@
+// Class Dynamo built to generate a pseudo-random slug for use in url
+/* BRIEF
+    This is my solution to preventing misuse of my API and protecting my keys.
+    Key is used in an env variable but it needs to be in the header sent to MAL
+    Sent request from server to MAL to prevent leaking the key but this the trick
+    this generates a NEW slug each time the SINGLE request this server accepts is made
+    this random slug that constantly changes is used in the url endpoint that triggers my API 
+        to send the request to MAL
+    Hopefully this prevents abuse
+*/
+
 class Dynammo {
     constructor() {
-        this.str = ""
+        this.str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
         this.num = 0
     }
 
     create(length) {
+        if (length === null || undefined) return
+        
+        if (length <= 0) {
+            return new Error({message: "ERROR: LEN MUST BE 1 OR HIGHER"})
+        } else if(length >= 100000) {
+            return new Error({message: "ERROR: LEN MUST NOT BE HIGHER THAN 99999"})
+        }
+
         let result = '';
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const characters = this.str;
         const charactersLength = characters.length;
         let counter = 0;
         while (counter < length) {
@@ -15,8 +34,6 @@ class Dynammo {
         }
         return result;
     }
-
-    verify() {}
 }
 
 module.exports = Dynammo
